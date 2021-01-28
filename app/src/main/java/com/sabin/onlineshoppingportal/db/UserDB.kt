9126929b870781e6ddfa01ae172975c2f38b1f1e
csabinji <57335447+csabinji@users.noreply.github.com>
2021-01-28 09:29:48 +0545

    Abstract Class is created in UserDB

--- app/src/main/java/com/sabin/onlineshoppingportal/db/UserDB.kt
+++ app/src/main/java/com/sabin/onlineshoppingportal/db/UserDB.kt
@@ -1,4 +1,34 @@
-package com.sabin.onlineshoppingportal.db
+package com.sabin.roomdatabaseactivity.db
 
-class UserDB {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.sabin.onlineshoppingportal.adapter.User
+import com.sabin.onlineshoppingportal.dao.UserDAO
+
+@Database(
+        entities = [(User::class)],
+        version = 1
+)
+abstract class StudentDB :RoomDatabase() {
+    abstract fun getUserDao() : UserDAO
+    companion object{
+        @Volatile
+        private var instance : StudentDB? = null
+        fun getInstance(context : Context): StudentDB{
+            if(instance==null){
+                synchronized(StudentDB::class){
+                    instance = buildDatabase(context)
+                }
+            }
+            return instance!!
+        }
+        private fun buildDatabase(context : Context) =
+                Room.databaseBuilder(
+                        context.applicationContext,
+                        StudentDB::class.java,
+                        "UserDB"
+                ).build()
+    }
 }
