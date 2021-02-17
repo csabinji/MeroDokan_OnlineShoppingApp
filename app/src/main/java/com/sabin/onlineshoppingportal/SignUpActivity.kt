bd58be906b9c5dbf528d982c4b0773ec91d89700
csabinji <57335447+csabinji@users.noreply.github.com>
2021-02-17 20:01:37 +0545

    Update in Signup

--- app/src/main/java/com/sabin/onlineshoppingportal/SignUpActivity.kt
+++ app/src/main/java/com/sabin/onlineshoppingportal/SignUpActivity.kt
@@ -14,8 +14,10 @@ import kotlinx.coroutines.withContext
 
 class SignUpActivity : AppCompatActivity(){
 
-    private lateinit var etxtName : EditText
+    private lateinit var etxtFname : EditText
+    private lateinit var etxtLname : EditText
     private lateinit var etxtEmail : EditText
+    private lateinit var etxtAddress : EditText
     private lateinit var etxtPhone : EditText
     private lateinit var etxtUser : EditText
     private lateinit var spnUser : Spinner
@@ -31,10 +33,12 @@ class SignUpActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        etxtName = findViewById(R.id.etxtName)
+        etxtFname = findViewById(R.id.etxtFname)
+        etxtLname = findViewById(R.id.etxtLname)
         etxtEmail = findViewById(R.id.etxtEmail)
         etxtPhone = findViewById(R.id.etxtPhone)
         etxtUser = findViewById(R.id.etxtUser)
+        etxtAddress = findViewById(R.id.etxtAddress)
         spnUser = findViewById(R.id.spnUser)
         etxtPass = findViewById(R.id.etxtPass)
         etxtRepass = findViewById(R.id.etxtRepass)
@@ -63,28 +67,40 @@ class SignUpActivity : AppCompatActivity(){
                 }
 
         btnSignup.setOnClickListener {
-            val name = etxtName.text.toString()
+            val fname = etxtFname.text.toString()
+            val lname = etxtLname.text.toString()
             val email = etxtEmail.text.toString()
-            val phone = etxtPhone.text.toString()
+            val phone = etxtPhone.text.toString().toInt()
+            val address = etxtAddress.text.toString()
             val username = etxtUser.text.toString()
-            val usertype = selectedItem
+            val accountType = selectedItem
             val password = etxtPass.text.toString()
             val repassword = etxtRepass.text.toString()
+
             if(password != repassword) {
                 etxtPass.error = "Password does not matched."
                 etxtPass.requestFocus()
                 return@setOnClickListener
             }else {
-                val user = User(name, email, phone, username, usertype, password )
+                val user = User(fname = fname, lname = lname, username = username, email = email, phone = phone, address = address, accountType = accountType, password = password )
+
                 CoroutineScope(Dispatchers.IO).launch {
-                    UserDB
-                            .getInstance(this@SignUpActivity)
-                            .getUserDao()
-                            .registerUser(user)
-                    withContext(Main){
-                        Toast.makeText(this@SignUpActivity, "Registered Sucessfully", Toast.LENGTH_SHORT).show()
+                    try {
+                        val userRepository = UserRepository()
+                        val response = userRepository.registerUser(user)
+                        if(response.success == true){
+                            withContext(Main){
+                                Toast.makeText(this@SignUpActivity, "Registered Successful", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                    }catch (ex: Exception){
+                        withContext(Main){
+                            Toast.makeText(this@SignUpActivity, "Username already taken", Toast.LENGTH_SHORT).show()
+                        }
                     }
                 }
+
             }
         }
     }
