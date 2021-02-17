f1228bc657bc91da3dcd7a49697bc0dfb1b4bacd
csabinji <57335447+csabinji@users.noreply.github.com>
2021-02-17 19:39:11 +0545

    User class updated

--- app/src/main/java/com/sabin/onlineshoppingportal/entity/User.kt
+++ app/src/main/java/com/sabin/onlineshoppingportal/entity/User.kt
@@ -1,17 +1,13 @@
 package com.sabin.onlineshoppingportal.adapter
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
 data class User (
-        var fullname : String? = null,
-        var email : String? = null,
-        var phone : String? = null,
+        var _id : String? = null,
+        var fname : String? = null,
+        var lname : String? = null,
         var username : String? = null,
-        var usertype : String? = null,
+        var email : String? = null,
+        var phone : Int? = null,
+        var address : String? = null,
+        var accountType : String? = null,
         var password : String? = null
-){
-    @PrimaryKey(autoGenerate = true)
-    var userId : Int = 0
-}
+)
