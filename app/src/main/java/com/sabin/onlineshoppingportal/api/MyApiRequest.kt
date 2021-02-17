4e9bd0ff8244e6e4bdee38624ce1a1e394ede09f
csabinji <57335447+csabinji@users.noreply.github.com>
2021-02-17 17:28:16 +0545

    Retrofit implemented in dependencies

--- app/src/main/java/com/sabin/onlineshoppingportal/api/MyApiRequest.kt
+++ app/src/main/java/com/sabin/onlineshoppingportal/api/MyApiRequest.kt
@@ -1,6 +1,8 @@
 package com.sabin.onlineshoppingportal.api
 
 import org.json.JSONException
+import org.json.JSONObject
+import retrofit2.Response
 import java.io.IOException
 
 abstract class MyApiRequest {
