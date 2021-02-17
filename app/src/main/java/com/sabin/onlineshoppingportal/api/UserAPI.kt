ec4a052192ec53681598ad6c00e0befecc108bfb
csabinji <57335447+csabinji@users.noreply.github.com>
2021-02-17 17:44:34 +0545

    Interface UserAPI created for User  Signup

---
+++ app/src/main/java/com/sabin/onlineshoppingportal/api/UserAPI.kt
@@ -0,0 +1,13 @@
+package com.sabin.onlineshoppingportal.api
+
+import com.sabin.onlineshoppingportal.adapter.User
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface UserAPI {
+    @POST("/signup")
+    suspend fun registerUser(
+        @Body user : User
+    ) : Response<LoginResponse>
+}
