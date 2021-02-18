027217a477cae4a11d6a57cceaccf9e1cee5d291
csabinji <57335447+csabinji@users.noreply.github.com>
2021-02-18 18:50:01 +0545

    New fun created in UserAPI for Login

--- app/src/main/java/com/sabin/onlineshoppingportal/api/UserAPI.kt
+++ app/src/main/java/com/sabin/onlineshoppingportal/api/UserAPI.kt
@@ -4,11 +4,23 @@ import com.sabin.onlineshoppingportal.adapter.User
 import com.sabin.onlineshoppingportal.response.LoginResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface UserAPI {
+
+    //SignUp
     @POST("/signup")
     suspend fun registerUser(
         @Body user : User
+    ) : Response<LoginResponse>
+
+    //Login
+    @FormUrlEncoded
+    @POST("/login")
+    suspend fun checkUser(
+            @Field("username") username : String,
+            @Field("password") password : String
     ) : Response<LoginResponse>
 }
