6cbbe1e54d5a94494d0a52842d0532c2337b41f8
csabinji <57335447+csabinji@users.noreply.github.com>
2021-02-27 22:25:56 +0545

    AddProduct response is created

--- app/src/main/java/com/sabin/onlineshoppingportal/api/ProductAPI.kt
+++ app/src/main/java/com/sabin/onlineshoppingportal/api/ProductAPI.kt
@@ -1,16 +1,18 @@
 package com.sabin.onlineshoppingportal.api
 
 import com.sabin.onlineshoppingportal.entity.Product
+import com.sabin.onlineshoppingportal.response.AddProductResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ProductAPI {
+
     //Add Product
     @POST("/product/insert")
     suspend fun addProduct(
         @Header("Authorization") token : String,
         @Body product : Product
-    ) : Response<AddP>
+    ) : Response<AddProductResponse>
 }
---
+++ app/src/main/java/com/sabin/onlineshoppingportal/response/AddProductResponse.kt
@@ -0,0 +1,8 @@
+package com.sabin.onlineshoppingportal.response
+
+import com.sabin.onlineshoppingportal.entity.Product
+
+data class AddProductResponse (
+    val success : Boolean? = null,
+    val data : Product? = null
+)
