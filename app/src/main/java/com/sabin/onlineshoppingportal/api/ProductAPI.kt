ad211309aa7582b7c418faf4834fbea7b3b411dd
csabinji <57335447+csabinji@users.noreply.github.com>
2021-02-27 22:23:16 +0545

    ProductAPI created

---
+++ app/src/main/java/com/sabin/onlineshoppingportal/api/ProductAPI.kt
@@ -0,0 +1,16 @@
+package com.sabin.onlineshoppingportal.api
+
+import com.sabin.onlineshoppingportal.entity.Product
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface ProductAPI {
+    //Add Product
+    @POST("/product/insert")
+    suspend fun addProduct(
+        @Header("Authorization") token : String,
+        @Body product : Product
+    ) : Response<AddP>
+}
