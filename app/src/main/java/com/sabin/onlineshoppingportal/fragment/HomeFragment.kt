9d1e682fd6fd244e192605acfb03245ee27acba6
csabinji <57335447+csabinji@users.noreply.github.com>
2021-03-01 20:45:25 +0545

    Displaying products using Room Database

--- app/src/main/java/com/sabin/onlineshoppingportal/fragment/HomeFragment.kt
+++ app/src/main/java/com/sabin/onlineshoppingportal/fragment/HomeFragment.kt
@@ -33,31 +33,65 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         topRecycler = view.findViewById(R.id.topRecycler)
         loadProduct()
+        getProduct()
         return view
     }
 
-    private fun loadProduct() {
+    private fun getProduct(){
+        try{
+            CoroutineScope(Dispatchers.IO).launch {
+                val lstProduct = ProductDB.getInstance(context!!).getProductDAO().getAllProduct()
+                withContext(Dispatchers.Main){
+                    val mLayoutManager = LinearLayoutManager(context)
+                    mLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+                    topRecycler.layoutManager = mLayoutManager
+                    topRecycler.adapter = ProductAdapter(lstProduct, context!!)
+                }
+            }
+        }catch (ex: Exception){
+            }
+        }
 
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val productRepository = ProductRepository()
-                val response = productRepository.getAllProducts()
+    private fun loadProduct(){
+//        try{
+//            CoroutineScope(Dispatchers.IO).launch {
+//                ProductDB.getInstance(context!!).getProductDAO().deleteAll()
+//                withContext(Dispatchers.Main){
+//                    val productRepository = ProductRepository()
+//                    val response = productRepository.getAllProducts()
+//                    response.data!!.forEach{
+//                    ProductDB.getInstance(context!!).getProductDAO().insertProduct(it)
+//                }
+//                }
+//            }
+//        }catch (ex: Exception){
+//
+//        }
+    }
+
+
+//    private fun loadProduct() {
+//
+//        CoroutineScope(Dispatchers.IO).launch {
+//            try {
+//                val productRepository = ProductRepository()
+//                val response = productRepository.getAllProducts()
 //                response.data!!.forEach{
 //                    ProductDB.getInstance(context!!).getProductDAO().insertProduct(it)
 //                }
-                if(response.success == true){
-                        val lstProduct = response.data!!
-                        val mLayoutManager = LinearLayoutManager(context)
-                        mLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
-                        topRecycler.layoutManager = mLayoutManager
-                        topRecycler.adapter = ProductAdapter(lstProduct, context!!)
-
-                }
-            }catch (ex: Exception){
-                withContext(Dispatchers.Main){
-                    Toast.makeText(getActivity(), "Error : ${ex.toString()}", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
+//                if(response.success == true){
+//                        val lstProduct = response.data!!
+//                        val mLayoutManager = LinearLayoutManager(context)
+//                        mLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+//                        topRecycler.layoutManager = mLayoutManager
+//                        topRecycler.adapter = ProductAdapter(lstProduct, context!!)
+//
+//                }
+//            }catch (ex: Exception){
+//                withContext(Dispatchers.Main){
+//                    Toast.makeText(getActivity(), "Error : ${ex.toString()}", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//        }
+//    }
 }
