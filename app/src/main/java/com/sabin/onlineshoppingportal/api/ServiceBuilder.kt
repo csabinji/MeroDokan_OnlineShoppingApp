e52882603f217f93dbb3751852fe528f60a232a5
csabinji <57335447+csabinji@users.noreply.github.com>
2021-04-17 15:24:32 +0545

    Light Sensor in Dashboard

--- app/src/main/java/com/sabin/onlineshoppingportal/api/ServiceBuilder.kt
+++ app/src/main/java/com/sabin/onlineshoppingportal/api/ServiceBuilder.kt
@@ -5,12 +5,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
-    private const val BASE_URL =
-        "http://10.0.2.2:90/"
+//    private const val BASE_URL =
+//        "http://10.0.2.2:90/"
 
 
 
-//    private const val BASE_URL = "http://192.168.137.114:90/"
+    private const val BASE_URL = "http://192.168.137.114:90/"
 
 
     var token: String? = null
